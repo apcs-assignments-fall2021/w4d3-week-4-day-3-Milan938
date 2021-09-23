db97ed471ce100fa0cf30c6825cc6723f8980091
milanboga <23mboga@athenian.org>
2021-09-23 12:27:42 -0700

    Finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -7,7 +7,11 @@ public class MyMain {
     //     4
     //     2
     public static void printEvensBackwards(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
         return;
     }
 
@@ -18,8 +22,14 @@ public class MyMain {
     //     isIncreasing([1, 1, 3, 6, 7]) => false (because 1 is not > 1)
     //     isIncreasing([1, 2, 3, 2, 4, 5, 8]) => false
     public static boolean isIncreasing(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
     // (Maybe Optional):
@@ -38,8 +48,15 @@ public class MyMain {
     // any duplicate values
     // Ex: containsDuplicates([1, 2, 3, 2, 1]) => true
     //     containsDuplicates([7, 2, 3, 4, 1, 5]) => false
+    // Compare one thing with everything else
     public static boolean containsDuplicates(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j] && i != j) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -48,7 +65,15 @@ public class MyMain {
     // to sort our code
     // This algorithm returns the sorted array
     public static int[] bubbleSort(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
+        for (int th = 0; th < arr.length; th++) {
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[i - 1]) {
+                    int willharris = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = willharris;
+                }
+            }
+        }
         return arr;
     }
 
@@ -79,7 +104,7 @@ public class MyMain {
 
         System.out.println();
         System.out.println("Bubble Sort Tests");
-        System.out.println(Arrays.toString(new int[] {6, 1, 3, 8, 2, 9})); // [1, 2, 3, 6, 8, 9]
-        System.out.println(Arrays.toString(new int[] {12, 5, 23, 74, 12, 86, 2})); // [2, 5, 12, 23, 74, 86]
+        System.out.println(Arrays.toString(bubbleSort(new int[] {6, 1, 3, 8, 2, 9}))); // [1, 2, 3, 6, 8, 9]
+        System.out.println(Arrays.toString(bubbleSort(new int[] {12, 5, 23, 74, 12, 86, 2}))); // [2, 5, 12, 12, 23, 74, 86]
     }
 }
